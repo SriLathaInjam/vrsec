3df31c5941b3e23e476fceade8ffd53e470da050
SriLathaInjam <injamsrilatha01@gmail.com>
2021-02-11 11:07:47 +0530

    odd

---
+++ odd.py
@@ -0,0 +1,15 @@
+  File "odd.py", line 1
+    File "odd.py", line 2
+    ^
+IndentationError: unexpected indent
+  File "odd.py", line 2
+    b=int(input("enter no")
+    ^
+SyntaxError: invalid syntax
+a=int(input("enter no")
+b=int(input("enter no")
+c=a+b
+print("c")
+bash: line 2: 2\: No such file or directory
+C:\ProgramData\Anaconda3\python.exe: can't open file 'odd.py': [Errno 2] No such file or directory
+
